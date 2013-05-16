d87a81914a1fea376c80b3450a52d1e347df5f81
Janne Leppänen <janne-leppanen@hotmail.com>
2013-05-16 22:01:35 +0300

    Added two libs, editor styles and some snippets

--- functions.php
+++ functions.php
@@ -5,13 +5,31 @@
 	/* Shh.. Don't tell them that we haven't updated WP */
 	remove_action( 'wp_head', 'wp_generator' );
 
+	/* Editor styling */
+	add_editor_style( '/_/css/editor.css' );
+
 	/* Scripts and styles */
 	add_action( 'wp_enqueue_scripts', 'wpbasetheme_register_scripts_and_styles' );
 	function wpbasetheme_register_scripts_and_styles() {
+		wp_register_script( 'modernizr', THEME_DIR . '/_/libs/modernizr.js', array(), 1.0, false );
 		wp_register_script( 'theme-functions', THEME_DIR . '/_/js/functions.js', array('jquery'), 1.0, true );
-		wp_register_style( 'theme-stylesheet', THEME_DIR . '/_/css/base.css', array(), 1.0, 'screen');
+		wp_register_style( 'normalize', THEME_DIR . '/_/libs/normalize.css', array(), 1.0, 'screen' );
+		wp_register_style( 'theme-stylesheet', THEME_DIR . '/_/css/base.css', array(), 1.0, 'screen' );
 
+		wp_enqueue_script( 'modernizr' );
 		wp_enqueue_script( 'theme-functions' );
+		wp_enqueue_style( 'normalize' );
 		wp_enqueue_style( 'theme-stylesheet' );
+	}
+
+	/* Find emails in content and filter them with antispambot */	
+	add_filter('the_content', 'wpbasetheme_antispambot_the_content_filter');
+	function wpbasetheme_antispambot_the_content_filter( $content ) {
+		$matches = array();
+		preg_match_all( "/\b\w+\@\w+[\.\w+]+\b/", $content, $matches);
+		foreach( $matches[0] as $match ){
+		  $content = str_replace( $match, antispambot( $match ), $content);
+		}
+		return $content;
 	}
 ?>
---
+++ readme.md
@@ -0,0 +1,6 @@
+# WP base theme
+Just another WordPress base theme which boosts developement for new themes.
+
+## Libraries
+- [Modernizr v2.6.2](http://modernizr.com/)
+- [Normalize v2.1.2](http://necolas.github.io/normalize.css/)
